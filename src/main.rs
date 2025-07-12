8905f94d9518aae6f89bafb60e5da5dd6c7a543c
goatastronaut0212 <goatastronaut0212@outlook.com>
2025-07-13 00:06:48 +0700

    Use config for database

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 use axum::{routing::get, Json, Router};
 use diesel::prelude::*;
-use dotenvy::dotenv;
-use std::env;
 
+use api::config::database::establish_connection;
 use api::models::tag::Tag;
 use api::schema::tag::dsl::*;
-
-pub fn establish_connection() -> PgConnection {
-    dotenv().ok();
-
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    PgConnection::establish(&database_url)
-        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
-}
 
 #[derive(serde::Serialize)]
 pub struct ApiResponse {
